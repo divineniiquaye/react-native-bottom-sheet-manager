9bd16f7881a10d2a9a776ddd37ac97bc95ad04ee
Divine Niiquaye Ibok <divineibok@gmail.com>
2025-11-02 18:49:15 +0000

    test: cover event manager behaviour

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["<rootDir>/src/**/__tests__/**/*.test.ts?(x)"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  globals: {
+    "ts-jest": {
+      tsconfig: "<rootDir>/tsconfig.json",
+    },
+  },
+  moduleNameMapper: {
+    "^@niibase/bottom-sheet-manager(?:/(.*))?$": "<rootDir>/src/$1",
+  },
+  clearMocks: true,
+};
