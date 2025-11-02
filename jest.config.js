37af14f16a3cdd10a2249314d6478f1ba75d86b2
Divine Niiquaye Ibok <divineibok@gmail.com>
2025-11-02 18:54:10 +0000

    fix: remove ts-jest globals & add events tests

--- jest.config.js
+++ jest.config.js
@@ -4,11 +4,6 @@ module.exports = {
   testEnvironment: "node",
   testMatch: ["<rootDir>/src/**/__tests__/**/*.test.ts?(x)"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  globals: {
-    "ts-jest": {
-      tsconfig: "<rootDir>/tsconfig.json",
-    },
-  },
   moduleNameMapper: {
     "^@niibase/bottom-sheet-manager(?:/(.*))?$": "<rootDir>/src/$1",
   },
